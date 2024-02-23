38379627441e0bc74822d545f02d970c82626422
Geferson Holdorf <gefersonholdorf88@gmail.com>
2024-02-23 15:17:00 -0300

    Firts class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) throws Exception {
         
-        System.out.println("Project Created");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
