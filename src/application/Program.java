beebcf35e8aebbe9cfb1876a76d16dd828deb1f3
Geferson Holdorf <gefersonholdorf88@gmail.com>
2024-02-24 17:49:45 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -46,7 +46,7 @@ public class Program {
             catch(InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
-            } 
+            }
         }
     }
 }
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -9,7 +9,7 @@ public class ChessPosition {
 
     public ChessPosition(char column, int row) {
         if(column < 'a' || column > 'h' || row < 1 || row > 8) {
-            throw new ChessException("Error instantiang ChessPosition. Valid value are from a1 to h8.");
+            throw new ChessException("Error instanting ChessPosition. Valid value are from a1 to h8.");
         }
         this.column = column;
         this.row = row;
@@ -28,7 +28,7 @@ public class ChessPosition {
     }
 
     protected static ChessPosition fromPosition(Position position) {
-        return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+        return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
     }
 
     @Override
